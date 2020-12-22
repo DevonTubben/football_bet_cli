42c5d00e13c6b47ebbf14d81ffb7758243ea3fd8
Devon Tubben <tubbend1@msu.edu>
2020-12-21 19:55:00 -0500

    added a new line to readme

---
+++ lib/football_bet_cli.rb
@@ -0,0 +1,6 @@
+require "football_bet_cli/version"
+
+module FootballBetCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/football_bet_cli/version.rb
@@ -0,0 +1,3 @@
+module FootballBetCli
+  VERSION = "0.1.0"
+end
